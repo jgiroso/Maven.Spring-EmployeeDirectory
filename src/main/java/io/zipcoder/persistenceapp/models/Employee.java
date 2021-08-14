014314f0b03c50359d3d49ed59c277a93464f464
jgiroso <jrgiroso@gmail.com>
2021-08-14 18:22:53 -0400

    scrapped and re-added employee class. not broken yet

---
+++ src/main/java/io/zipcoder/persistenceapp/models/Employee.java
@@ -0,0 +1,93 @@
+package io.zipcoder.persistenceapp.models;
+
+
+import java.sql.Date;
+
+public class Employee {
+
+    Long id;
+    String firstName;
+    String lastName;
+    String title;
+    String phoneNumber;
+    String email;
+    Date hireDate;
+    Employee manager;
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String firstName, String lastName, String title, String phoneNumber, String email, Date hireDate) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.title = title;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.hireDate = hireDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+}
