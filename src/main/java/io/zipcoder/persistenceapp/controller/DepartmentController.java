59ce99d83fdb7c77a1d5b1ff9097f77fc605e087
jgiroso <jrgiroso@gmail.com>
2021-08-15 14:38:54 -0400

    basic controller classes created

--- src/main/java/io/zipcoder/persistenceapp/controller/DepartmentController.java
+++ src/main/java/io/zipcoder/persistenceapp/controller/DepartmentController.java
@@ -5,9 +5,7 @@ import io.zipcoder.persistenceapp.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/API")
@@ -16,8 +14,29 @@ public class DepartmentController {
     @Autowired
     DepartmentService service;
 
-    @PostMapping("/create/dept")
+    @PostMapping("/dept/create")
     public ResponseEntity<Department> create(Department department) {
         return new ResponseEntity<>(service.create(department), HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/dept/{id}")
+    public ResponseEntity<Department> findById(@PathVariable Long id) {
+        return new ResponseEntity<>(service.findById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/dept/depts")
+    public ResponseEntity<Iterable<Department>> findAll() {
+        return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
+    }
+
+    @PutMapping("/dept/update/{id}")
+    public ResponseEntity<Department> update(@PathVariable Long id, @RequestBody Department department) {
+        return new ResponseEntity<>(service.update(id, department), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/dept/delete/{id}")
+    public ResponseEntity<Department> delete(@PathVariable Long id) {
+        return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
 }
--- src/main/java/io/zipcoder/persistenceapp/controller/EmployeeController.java
+++ src/main/java/io/zipcoder/persistenceapp/controller/EmployeeController.java
@@ -5,9 +5,7 @@ import io.zipcoder.persistenceapp.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/API")
@@ -16,9 +14,28 @@ public class EmployeeController {
     @Autowired
     EmployeeService service;
 
-    @PostMapping("/create/employee")
-    public ResponseEntity<Employee> create(Employee employee) {
+    @PostMapping("/employee/create")
+    public ResponseEntity<Employee> create(@RequestBody Employee employee) {
         return new ResponseEntity<>(service.create(employee), HttpStatus.CREATED);
     }
 
+    @GetMapping("/employee/{id}")
+    public ResponseEntity<Employee> findById(@PathVariable Long id) {
+        return new ResponseEntity<>(service.findById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/employee/employees")
+    public ResponseEntity<Iterable<Employee>> findAll() {
+        return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
+    }
+
+    @PutMapping("/employee/update/{id}")
+    public ResponseEntity<Employee> update(@PathVariable Long id, @RequestBody Employee employee) {
+        return new ResponseEntity<>(service.update(id, employee), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/employee/delete/{id}")
+    public ResponseEntity<Employee> delete(@PathVariable Long id) {
+        return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
+    }
 }
